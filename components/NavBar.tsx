28571a3bcb005e2e7c364153b70128408ab9932d
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-01-05 00:33:31 +0100

    add tailwind css

---
+++ components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React, { ReactFragment } from 'react'
+
+
+
+const NavBar: React.FC= () => {
+  return (
+    <div className='bg-black'>
+      <h1 className='text-red-800'>Hello!</h1>
+    </div>
+  );
+}
+
+
+export default NavBar;
