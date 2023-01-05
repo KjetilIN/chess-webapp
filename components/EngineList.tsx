9121ea890a25b2620316125a6a48723509a3e6cf
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-01-05 02:55:49 +0100

    add styling to main page

--- components/EngineList.tsx
+++ components/EngineList.tsx
@@ -18,7 +18,7 @@ interface Props {
 export const EngineList: React.FC<Props> = ({ listOfEngine }) => {
     return (
 
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg mb-5">
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <caption className="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
                     Our engines
@@ -48,15 +48,15 @@ export const EngineList: React.FC<Props> = ({ listOfEngine }) => {
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {engine.name}
                             </th>
-                            <td className="px-6 py-4">
+                            <td className="px-6 py-4 italic">
                                 {engine.version}
                             </td>
-                            <td className="px-6 py-4">
+                            <td className="px-6 py-4 font-bold">
                                 {engine.deciption}
                             </td>
                             <td className="px-6 py-4">
 
-                                <Link href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">To engine!</Link>
+                                <Link href={engine.link} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">To engine!</Link>
 
                             </td>
                         </tr>)
--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className='bg-gray-400'>
 
         <div className='flex flex-col items-center gap-20'>
           <section className='w-2/3 bg-gray-900 flex flex-col items-center justify-center mt-20 rounded-xl p-4'>
@@ -68,7 +68,7 @@ export default function Home() {
           <blockquote className="text-xl italic font-semibold text-gray-900">
             <svg aria-hidden="true" className="w-10 h-10 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor" /></svg>
             <p>"These people have no clue how to make a good chess engine."</p>
-            <h5 className='mt-2'>Bill Gates</h5>
+            <h5 className='mt-2 text-white font-bold border-black'>Bill Gates</h5>
           </blockquote>
 
 
