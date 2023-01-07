79f1dfcd4e2842150d288485b0500bb127e064f8
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-01-07 22:32:03 +0100

    add: setup test file with jest for running tests

---
+++ modules/test/ChessRules.test.ts
@@ -0,0 +1,11 @@
+
+
+export{} //Remove this 
+
+describe("Write the title of the test", () =>{
+
+    test("Write the title of the specific test case", ()=>{
+        expect(true).toBe(true);
+    })
+})
+
