adc0fd758d5a7f04a30262f2da074dc6cdfbbe07
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-01-08 16:04:21 +0100

    add: template functions in ChessRules.ts

--- modules/ChessRules.ts
+++ modules/ChessRules.ts
@@ -1,4 +1,44 @@
-export{}
+export { isWhiteInCheck, isBlackInCheck, canWhiteCastle, canBlackCastle};
 
-//This is the file with all chess rules 
+/**
+ * Function that checks if white is in check.
+ *
+ * @param board the state of the board as a 2D array
+ * @returns true if white is in check
+ */
+function isWhiteInCheck(board: string[]): boolean {
+    return false;
+}
 
+/**
+ * Function that checks if black is in check.
+ *
+ * @param board the state of the board as a 2D array
+ * @returns true if blaxk is in check
+ */
+function isBlackInCheck(board: string[]): boolean {
+    return false;
+}
+
+/**
+ * Check if white can castle
+ * @param moves List of moves
+ * @param board current state of board
+ * @returns returns true if white can castle
+ *
+ */
+
+function canWhiteCastle(moves: string[], board: string[]): boolean {
+    return false;
+}
+
+/**
+ * Check if black can castle
+ * @param moves List of moves
+ * @param board current state of board
+ * @returns returns true if white can castle
+ *
+ */
+function canBlackCastle(moves: string[], board: string[]): boolean {
+    return false;
+}
