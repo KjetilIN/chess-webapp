2b860d8971c1f2f0c204e02ee9cc89ae86f19083
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-01-11 10:49:21 +0100

    fix: tconfig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/Global.css'
-import type { AppProps } from 'next/app'
 import NavBar from '../components/NavBar'
 import Head from 'next/head'
+import { AppProps } from 'next/app'
+import React from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
